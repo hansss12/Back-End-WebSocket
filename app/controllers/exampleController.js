101e3f241a9c4ab8bb6f31eb631f5dadf25a114e
hansss12 <raihan.aqil2003@gmail.com>
2023-07-03 16:39:09 +0700

    fix : add longitide and latitude to api

--- app/controllers/exampleController.js
+++ app/controllers/exampleController.js
@@ -1,3 +1,5 @@
+const dataLatLon = require("../../files/latlon");
+
 exports.callmeWebSocket = async (req, res) => {
   // endpoint livethreat tidak mereturn latlon
   // jadi kamu harus melookup latlon tersebut 
@@ -5,7 +7,23 @@ exports.callmeWebSocket = async (req, res) => {
   try {
     const response = await fetch('https://livethreatmap.radware.com/api/map/attacks?limit=10');
     const data = await response.json();
-    res.status(200).json(data)
+    let newData = []
+    for (let i = 0; i < dataLatLon.length; i++) {
+      const element = dataLatLon[i];
+      for (let index = 0; index < data.length; index++) {
+        const newEl = data[index];        
+        await newEl.forEach((el) => {
+          if (el.sourceCountry == element.iso_2) {
+            newData.push({
+                ...el,
+                latitude: element.latitude,
+                longitude: element.longitude
+              })
+          } 
+        })
+      }
+    }
+    res.status(200).json(newData)
   } catch (error) {
     console.log(error)
     res.status(500).json({
